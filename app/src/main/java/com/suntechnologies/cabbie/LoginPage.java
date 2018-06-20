ce5c65ea6e6e3e0bc1b292276d813e1c37726e99
Hareesh S <hareeshs@suntechnologies.com>
2018-06-20 08:47:03 +0530

    Created layouts for login and signup pages.

--- app/src/main/java/com/suntechnologies/cabbie/LoginPage.java
+++ app/src/main/java/com/suntechnologies/cabbie/LoginPage.java
@@ -10,6 +10,7 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.login_page);
     }
 
